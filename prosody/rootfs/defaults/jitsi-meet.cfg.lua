8b477e99f97985ab3bf9cc0581e7948924ccf65e
Sergey Tereshchenko <tor2next@yandex.ru>
2025-03-22 21:39:29 +0700

    Update jitsi-meet.cfg.lua

--- prosody/rootfs/defaults/jitsi-meet.cfg.lua
+++ prosody/rootfs/defaults/jitsi-meet.cfg.lua
@@ -4,50 +4,46 @@ Component "internal-muc.meet.jitsi" "muc"
     muc_room_locking = false
     muc_room_default_public_jids = true
     modules_enabled = { "muc_hide_all"; "muc_filter_access"; }
-    muc_filter_whitelist = "${XMPP_AUTH_DOMAIN}"
+    muc_filter_whitelist = "auth.meet.jitsi"
 
-VirtualHost "${XMPP_RECORDER_DOMAIN}"
+VirtualHost "recorder.meet.jitsi"
     authentication = "internal_hashed"
     ssl = {
-        key = "/config/certs/${XMPP_RECORDER_DOMAIN}.key";
-        certificate = "/config/certs/${XMPP_RECORDER_DOMAIN}.crt";
+        key = "/config/certs/recorder.meet.jitsi.key";
+        certificate = "/config/certs/recorder.meet.jitsi.crt";
     }
 
-Component "conference.joinrm-svz.ru" "muc"
+Component "muc.meet.jitsi" "muc"
+    restrict_room_creation = true
+    storage = "memory"
+    modules_enabled = { "muc_meeting_id"; "polls"; "muc_domain_mapper"; }
+    muc_room_locking = false
+    muc_room_default_public_jids = true
+
+Component "focus.meet.jitsi" "client_proxy"
+    target_address = "focus@auth.meet.jitsi"
+
+Component "speakerstats.meet.jitsi" "speakerstats_component"
+    muc_component = "muc.meet.jitsi"
+
+Component "conferenceduration.meet.jitsi" "conference_duration_component"
+    muc_component = "muc.meet.jitsi"
+
+Component "endconference.meet.jitsi" "end_conference"
+    muc_component = "muc.meet.jitsi"
+
+Component "avmoderation.meet.jitsi" "av_moderation_component"
+    muc_component = "muc.meet.jitsi"
+
+Component "lobby.meet.jitsi" "muc"
     storage = "memory"
     restrict_room_creation = true
     muc_room_locking = false
     muc_room_default_public_jids = true
-    modules_enabled = { "muc_meeting_id"; "muc_domain_mapper"; "polls"; }
 
-Component "lobby.joinrm-svz.ru" "muc"
-    storage = "memory"
-    restrict_room_creation = true
-    muc_room_locking = false
-    muc_room_default_public_jids = true
-
-Component "breakout.joinrm-svz.ru" "muc"
+Component "breakout.meet.jitsi" "muc"
     storage = "memory"
     restrict_room_creation = true
     muc_room_locking = false
     muc_room_default_public_jids = true
     modules_enabled = { "muc_meeting_id"; "polls"; }
-
-Component "speakerstats.joinrm-svz.ru" "speakerstats_component"
-    muc_component = "muc.joinrm-svz.ru"
-
-Component "conferenceduration.joinrm-svz.ru" "conference_duration_component"
-    muc_component = "muc.joinrm-svz.ru"
-
-Component "endconference.joinrm-svz.ru" "end_conference"
-    muc_component = "muc.joinrm-svz.ru"
-
-Component "avmoderation.joinrm-svz.ru" "av_moderation_component"
-    muc_component = "muc.joinrm-svz.ru"
-
-Component "muc.joinrm-svz.ru" "muc"
-    storage = "memory"
-    restrict_room_creation = true
-    muc_room_locking = false
-    muc_room_default_public_jids = true
-    modules_enabled = { "muc_meeting_id"; "muc_domain_mapper"; "polls"; }
