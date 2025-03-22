cceeb62658e7b958a6edafc07e547f9311b2f72d
Sergey Tereshchenko <tor2next@yandex.ru>
2025-03-22 21:13:09 +0700

    Update jitsi-meet.cfg.lua

--- prosody/rootfs/defaults/jitsi-meet.cfg.lua
+++ prosody/rootfs/defaults/jitsi-meet.cfg.lua
@@ -12,3 +12,35 @@ VirtualHost "${XMPP_RECORDER_DOMAIN}"
         key = "/config/certs/${XMPP_RECORDER_DOMAIN}.key";
         certificate = "/config/certs/${XMPP_RECORDER_DOMAIN}.crt";
     }
+
+Component "conference.joinrm-svz.ru" "muc"
+    storage = "memory"
+    restrict_room_creation = true
+    muc_room_locking = false
+    muc_room_default_public_jids = true
+    modules_enabled = { "muc_meeting_id"; "muc_domain_mapper"; "polls"; }
+
+Component "lobby.joinrm-svz.ru" "muc"
+    storage = "memory"
+    restrict_room_creation = true
+    muc_room_locking = false
+    muc_room_default_public_jids = true
+
+Component "breakout.joinrm-svz.ru" "muc"
+    storage = "memory"
+    restrict_room_creation = true
+    muc_room_locking = false
+    muc_room_default_public_jids = true
+    modules_enabled = { "muc_meeting_id"; "polls"; }
+
+Component "speakerstats.joinrm-svz.ru" "speakerstats_component"
+    muc_component = "muc.joinrm-svz.ru"
+
+Component "conferenceduration.joinrm-svz.ru" "conference_duration_component"
+    muc_component = "muc.joinrm-svz.ru"
+
+Component "endconference.joinrm-svz.ru" "end_conference"
+    muc_component = "muc.joinrm-svz.ru"
+
+Component "avmoderation.joinrm-svz.ru" "av_moderation_component"
+    muc_component = "muc.joinrm-svz.ru"
